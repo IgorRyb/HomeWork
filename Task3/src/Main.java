4add4dccb768053113bd45f90c3c06b831f94feb
Admin <rvbchenko@mail.ru>
2023-02-17 01:57:07 +0500

    Исправленная ДЗ по проектированию ООП-модели

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,64 +1,74 @@
+
 import java.util.Scanner;
 
 public class Main {
-    public static int trueCount = 0, falseCount = 0;
 
     public static void main(String[] args) {
-        Questions que = new Questions();
         Answers ans = new Answers();
-        String[] questions = Questions.arrayQuestions;
-        for (int i = 0; i < questions.length; i++) {
-            que.typeQuestions(i);
-            int b = 0;
-            ans.OutputOfAnswerOptions(b, i);
-            int userResponse = que.userInput();
-            boolean TrueAnswer = ans.checkingForCorrect(i, userResponse);
-            if (TrueAnswer) {
-                System.out.println(userResponse + " - Ответ верный");
-                trueCount++;
-            } else {
-                System.out.println(userResponse + " - Ответ неверный");
-                falseCount++;
-            }
-            System.out.println();
-        }
-        System.out.println("Результат: Правильных ответов " + trueCount + " , Неправильных ответов " + falseCount);
-
+        Questions que = new Questions(ans);
+        Test test = new Test(que);
+        test.passTest();
     }
 
     public static class Questions {
-        static String[] arrayQuestions = {"а) Сколько существует примитивных типов в языке Java?", " б) Как называется состояние репозитория в Git на какой-то момент времени?", "в) Как называется модификатор для создания константы?"};
 
-        static void typeQuestions(int n) {
-            System.out.println(arrayQuestions[n] + " Варианты ответов:");
+        private final static String[] arrayQuestions = {"а) Сколько " +
+                "существует примитивных типов в языке Java?", " б) Как называется состояние репозитория в Git на какой-то момент времени?", "в) Как называется модификатор для создания константы?"};
+        static boolean True;
+        static int userInput;
+        private static Answers ans;
+
+        public Questions(Answers ans) {
+            this.ans = ans;
         }
 
-        public static int userInput() {
-            Scanner scan = new Scanner(System.in);
-            int userInput = scan.nextInt();
-            return userInput;
+        static boolean ask(int n) {
+                int b = 0;
+                System.out.println(arrayQuestions[n] + " Варианты ответов:");
+                while (b < ans.arrayAnswer[n].length) {
+                    System.out.println(ans.arrayAnswer[n][b]);
+                    b++;
+                }
+                Scanner scan = new Scanner(System.in);
+                userInput = scan.nextInt();
+                if (userInput == ans.arrayTrueAnswers[n] + 1) {
+                    True = true;
+                } else {
+                    True = false;
+                }
+            return True;
         }
+
     }
 
     public static class Answers {
-        int[] arrayTrueAnswers = {3, 2, 2};
-        static String[][] arrayAnswer = {{"1. Один", "2. Два", "3. Три", "4. Четыре", "5. Пять"}, {"1. Pull-up", "2. Pull Request", "3. Commit", "4. Branch"}, {"1. static", "2. void", "3. final", "4. public"}};
-        boolean True;
+        private static int[] arrayTrueAnswers = {3, 2, 2};
+        private static String[][] arrayAnswer = {{"1. Один", "2. Два", "3. Три", "4. Четыре", "5. Пять"}, {"1. Pull-up", "2. Pull Request", "3. Commit", "4. Branch"}, {"1. static", "2. void", "3. final", "4. public"}};
 
-        public void OutputOfAnswerOptions(int b, int p) {
-            while (b < arrayAnswer[p].length) {
-                System.out.println(arrayAnswer[p][b]);
-                b++;
-            }
+
+    }
+
+    public static class Test {
+        static int trueCount = 0, falseCount = 0;
+        private static Questions quest;
+
+        public Test(Questions quest) {
+            this.quest = quest;
         }
 
-        public boolean checkingForCorrect(int n, int p) {
-            if (p == arrayTrueAnswers[n] + 1) {
-                True = true;
-            } else {
-                True = false;
+        static void passTest() {
+            for (int i=0; i < quest.arrayQuestions.length; i++) {
+                if (quest.ask(i)) {
+                    System.out.println(quest.userInput + "- это верный ответ");
+                    trueCount++;
+                }
+                else {
+                    System.out.println("Ваш ответ: " + quest.userInput + " - это неверный ответ");
+                    falseCount++;
+                }
+                System.out.println("");
             }
-            return True;
+            System.out.println("Результат: Правильных ответов " + trueCount + " , Неправильных ответов " + falseCount);
         }
     }
 }
